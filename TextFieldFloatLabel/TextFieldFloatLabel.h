b6596c7b176db80987aa4642e32546c8de88440c
Vasyl Fuchenko <vasyl.fuchenko@globallogic.com>
2020-01-21 22:53:24 +0200

    Initial project setup

---
+++ TextFieldFloatLabel/TextFieldFloatLabel.h
@@ -0,0 +1,19 @@
+//
+//  TextFieldFloatLabel.h
+//  TextFieldFloatLabel
+//
+//  Created by Vasyl on 1/21/20.
+//  Copyright © 2020 VasylFuchenko. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for TextFieldFloatLabel.
+FOUNDATION_EXPORT double TextFieldFloatLabelVersionNumber;
+
+//! Project version string for TextFieldFloatLabel.
+FOUNDATION_EXPORT const unsigned char TextFieldFloatLabelVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <TextFieldFloatLabel/PublicHeader.h>
+
+
